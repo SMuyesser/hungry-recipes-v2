b62b7f54adf907ef14fe6c4f75716a27cdcb2006
SMuyesser <smuyesser@gmail.com>
2017-04-22 15:49:14 -0700

    no results message added, current ingredients now fixed to bottom

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ function getSearchFromApi(searchTerm, callback) {
   $.getJSON(SearchRequest_URL, query, callback);
 }
 
-//next page of results
+//show more recipes
 var page=1;
 $('body').on('click', 'button#show-more-recipe-btn', function(event) {
   var searchTerm = $('#title-searchbar').find('.js-query').val();
@@ -173,21 +173,51 @@ $('body').on('click', 'button#show-more-recipe-btn', function(event) {
   });
 })
 
+//show more ingredient search recipes
+$('body').on('click', 'button#show-more-ingRecipe-btn', function(event) {
+  var searchTerm = selected.toString();
+  page += 1;
+  var query = {
+    q: searchTerm,
+    page: page,
+  }
+  $.getJSON(SearchRequest_URL, query, function(data) {
+    var displayElem = $('.js-search-results');
+    var recipes = data.recipes.map(function(recipe) {
+      var elem = $('.js-result-template').children().clone();
+      var imageUrl = recipe.image_url;
+      var sourceUrl = recipe.source_url;
+      var recipeName = recipe.title;
+      elem.find('h5').html(recipeName);
+      elem.find('img').attr('src', imageUrl);
+      elem.find('a').attr('href', sourceUrl);
+      return elem;
+    });
+    displayElem.append(recipes);
+  });
+})
+
 //display search results
 function displaySearchData(data) {
   var displayElem = $('.js-search-results');
-  var recipes = data.recipes.map(function(recipe) {
-    var elem = $('.js-result-template').children().clone();
-    var imageUrl = recipe.image_url;
-    var sourceUrl = recipe.source_url;
-    var recipeName = recipe.title;
-    elem.find('h5').html(recipeName);
-    elem.find('img').attr('src', imageUrl);
-    elem.find('a').attr('href', sourceUrl);
-    return elem;
-  });
-  displayElem.html(recipes);
-  $('div#recipe-search-buttons').removeClass('hidden');
+  if (data.count === 0) {
+    var searchFor = $('.js-query').val();
+    var noResults = "<h2>No recipes for " + searchFor + " were found.  How about trying to find a different recipe?</h2>"
+    $('div.js-search-results').append(noResults);
+  } else {
+    var recipes = data.recipes.map(function(recipe) {
+      var elem = $('.js-result-template').children().clone();
+      var imageUrl = recipe.image_url;
+      var sourceUrl = recipe.source_url;
+      var recipeName = recipe.title;
+      elem.find('h5').html(recipeName);
+      elem.find('img').attr('src', imageUrl);
+      elem.find('a').attr('href', sourceUrl);
+      return elem;
+    });
+    displayElem.html(recipes);
+    $('div#recipe-search-buttons').removeClass('hidden');
+  }
 }
 
 function clearResults() {
